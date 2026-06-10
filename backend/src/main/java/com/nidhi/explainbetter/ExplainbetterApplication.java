40ab6100bf1fb6f4a53a5a165e2cfa8422157ec5
Nidhi Thakkar <nidhithakkar002@gmail.com>
2026-06-10 22:38:59 +0530

    Initialize Spring Boot backend

---
+++ backend/src/main/java/com/nidhi/explainbetter/ExplainbetterApplication.java
@@ -0,0 +1,13 @@
+package com.nidhi.explainbetter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplainbetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExplainbetterApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/nidhi/explainbetter/ExplainbetterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nidhi.explainbetter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplainbetterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
